dd3d4f1bfd42ef8e7a6594897899f56e31a1096b
Dumnezeu1 <47244244+Dumnezeu1@users.noreply.github.com>
2021-02-07 18:41:24 +0200

    heroku setup

--- server.js
+++ server.js
@@ -3,17 +3,27 @@ const connectDB = require("./config/db");
 
 const app = express();
 
+const path = require("path");
+
 connectDB();
 
 // Middleware
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "Server running" }));
+// app.get("/", (req, res) => res.json({ msg: "Server running" }));
 
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
